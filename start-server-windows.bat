6cd7bc1317625fcfe8703e302230ab0974468f6e
Joseph Hotchkiss <joe.hotchkiss@gmail.com>
2026-06-28 20:15:07 +0300

    v0.12.5 Add server launcher

--- start-server-windows.bat
+++ start-server-windows.bat
@@ -1,3 +1,9 @@
 @echo off
+setlocal
 cd /d "%~dp0"
-py -3 scripts\flora-server.py --host 127.0.0.1 --port 8000
+
+python scripts\flora-launcher.py %*
+if %errorlevel% equ 0 exit /b 0
+
+py -3 scripts\flora-launcher.py %*
+exit /b %errorlevel%
---
+++ start-server-wine.bat
@@ -0,0 +1,2 @@
+@echo off
+start /unix /usr/bin/python /home/vze3f372/Documents/streamerbot/streampanel/scripts/flora-launcher.py
