56b4cb99d0f712c155eb205df991e53d5f7bd57d
vze3f372 <16195072+vze3f372@users.noreply.github.com>
2026-06-29 16:54:28 +0300

    v0.15.2 Stop Flora when Streamer.bot closes

--- start-server-wine.bat
+++ start-server-wine.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d Z:\home\vze3f372\Documents\streamerbot\streampanel
-start /unix /usr/bin/bash /home/vze3f372/Documents/streamerbot/streampanel/start-server-linux.sh %*
+start /unix /usr/bin/bash /home/vze3f372/Documents/streamerbot/streampanel/start-server-linux.sh --watch-streamerbot %*
 exit /b 0
